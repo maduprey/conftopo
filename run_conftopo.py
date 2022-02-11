3609c693fa5feefd29262d372a8b1373c48e0bfb
Duprey, Michael <mduprey@rti.org>
2022-02-11 12:35:28 -0700

    Update run_conftopo.py

--- run_conftopo.py
+++ run_conftopo.py
@@ -24,23 +24,26 @@ def compute_conf_pc(dir=None):
     """
 
     # Loop over PDB files in directory
-    os.chdir(dir)
-    n_files = len(os.listdir())
+    # os.chdir(dir)
+    n_files = len(os.listdir(dir))
     lcs = np.empty((0, 300), int)
-    for i in range(1, n_files):
-        lc_all = compute_persist_curves('morph_'+str(i)+'.pdb', verbose=1)
+    for i in range(1, n_files+1):
+        lc_all = compute_persist_curves(dir+'/morph_'+str(i)+'.pdb', verbose=1)
         lcs = np.vstack((lcs, lc_all))
 
     return(lcs)
 
 
 def main():
-    get_proteins('1klq', '1duj')
+    mol_1 = '2e2n'
+    mol_2 = '2e2q'
+
+    get_proteins(mol_1, mol_2)
     chimerax()
     print(os.getcwd())
     lcs = compute_conf_pc('conftopo/data/tmp/morph')
 
-    # Compute across H_0, H_1, and H_2
+    # Set plotting range to H_0, H_1, and H_2
     homology = range(000, 300)
 
     # Compute L^2 norm
@@ -49,13 +52,14 @@ def main():
         dist.append(np.linalg.norm(lcs[0, homology] - lcs[k, homology]))
 
     # Plot
+    plt.figure()
     plt.subplots(figsize=(12, 6))
-    plt.title("$L^2$ norm between persistence curves for conformations)")
+    plt.title(
+        "$L^2$ norm between persistence curves for conformations "+mol_1+" to "+mol_2)
     plt.xlabel("Conformation")
     plt.ylabel("$L^2$ norm")
     plt.plot(dist)
-    # plt.show()
-    plt.savefig('conftopo/data/graph.png')
+    plt.savefig('conftopo/data/norm_plot_'+mol_1+"_"+mol_2+'.png')
 
 
 if __name__ == '__main__':
